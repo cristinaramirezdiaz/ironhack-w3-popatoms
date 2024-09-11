35977b28a3f61a8034f5eb354bf5a5050ba10110
Almudena M. Castro <almu@Almudenas-MacBook-Pro.local>
2024-09-12 01:05:21 +0200

    edit functions to keep results in csv

--- functions.py
+++ functions.py
@@ -7,6 +7,9 @@ from datetime import date, timedelta
 import seaborn as sns
 import pprint
 import matplotlib.pyplot as plt
+import time
+import numpy as np
+
 
 # Load environment variables
 load_dotenv()
@@ -88,6 +91,7 @@ def get_track_genre(id,sp=sp):
     dict
         Audio features of the track.
     """
+
     result= sp.audio_features(id)[0]
     return {
         'danceability': result['danceability'],
@@ -101,14 +105,16 @@ def get_track_genre(id,sp=sp):
         'liveness': result['liveness'],
         'valence': result['valence'],
         'tempo': result['tempo'],
+        'duration': result['duration_ms']
     }
 
 
 # He dejado esos valores ahí por si nos interesa llamar al confidence
 # o investigar si el fade out se ha pasado de moda ?¿?¿
-def get_audio_analysis(id,sp=sp):
+'''
+ def get_audio_analysis(id,sp=sp):
 
-    result = sp.audio_analysis(id)['track']
+#   result = sp.audio_analysis(id)['track']
 #  'start_of_fade_out': 171.68254,
 #  'tempo': 173.988,
 #  'tempo_confidence': 0.125,
@@ -117,8 +123,10 @@ def get_audio_analysis(id,sp=sp):
 #  'mode': 1,
 #  'mode_confidence': 0.512,
     return {'duration': result['duration']}
+'''
 
-
+'''
+# Deprecated
 def create_multiple_tracks_df(songs_list):
     # Lista de diccionarios de cada canción 
     track_info_list = [] 
@@ -164,7 +172,7 @@ def create_multiple_tracks_df(songs_list):
     
     # Convertir la lista de diccionarios en un DataFrame
     return pd.DataFrame(track_info_list)
-
+'''
 
 def list_dates_in_range(start_dt, end_dt, step=7):
      
@@ -198,26 +206,39 @@ def list_dates_in_range(start_dt, end_dt, step=7):
         print("Start and end dates must be in iso format YYYY-MM-DD")
 
 
-def create_weekly_ranks_df(start_dt, end_dt, chart_name = "hot-100"):
+def create_weekly_ranks_df(start_dt, end_dt, chart_name = "hot-100", csv_path="", step=30):
 
-    dates = list_dates_in_range(start_dt, end_dt, step=7)
+     # if there's a csv file, we can use it as starting point. 
+    if csv_path != "":
+        df = pd.read_csv(csv_path)
+        start_dt = str(date.fromisoformat(df.date.max()) + timedelta(days=7))
+        i=len(df)
+    
+    else: 
+        df = pd.DataFrame(columns = ["date", "title", "artist", "peak_pos", "rank", "weeks"])
+        i=0
 
-    charts_list = []
 
-    for date in dates:
-        chart = billboard.ChartData(chart_name, date=date)
+    dates = list_dates_in_range(start_dt, end_dt, step)
+    
+
+    for value in dates:
+ 
+        chart = billboard.ChartData("hot-100", date='1961-08-18')
         for entry in chart:
-            entry_dict = {'date': date, 
-                          'title': entry.title, 
-                          'artist': entry.artist,
-                          'peakPos': entry.peakPos, 
-                          'rank': entry.rank,
-                          'lastPos': entry.lastPos,
-                          'weeksOnChart': entry.weeks
-                          }
-            charts_list.append(entry_dict)
+            df.loc[i, "date"] = value
+            df.loc[i, "title"] = entry.title
+            df.loc[i, "artist"] = entry.artist
+            df.loc[i, "peak_pos"] = entry.peakPos
+            df.loc[i, "rank"] = entry.rank
+            df.loc[i, "weeks"] = entry.weeks
+            df.to_csv('weekly_rankings_temp.csv', index=False)
+            i+=1
 
-    return pd.DataFrame(charts_list)
+    # concatenate temp_df with df_temp if there's a csv file
+
+    return df
+
 
 # Filter duplicates
 
@@ -239,9 +260,8 @@ def calculate_rank_peak_data(df):
     # Group by the title of the song
     grouped = df.groupby('title').agg(
         artist = ('artist', 'first'),  # Get the first artist
-        peak_pos=('peak_pos', 'max'),  # Get the maximum peak position
         weeks=('weeks', 'max'),         # Get the maximum number of weeks
-        paak_rank=('rank', 'min')         # Get the maximum rank
+        peak_rank=('rank', 'min')         # Get the maximum rank
     ).reset_index()
 
     # Find the date when the max peak_pos was recorded
@@ -251,3 +271,52 @@ def calculate_rank_peak_data(df):
     result = pd.merge(grouped, peak_date, on='title')
     
     return result
+
+def create_audio_analysis_df(df):
+
+    # if there's a csv file, we can use it as starting point.
+    if "id" not in df.columns:
+        df["id"] = np.nan
+
+    # Lista de diccionarios de cada canción 
+
+    for i in range(len(df)):
+        if pd.isna(df.loc[i,"id"]):
+            try:
+                song = df.loc[i,"title"] + " " + df.loc[i,"artist"]
+
+                # Info de la canción
+                track_details = get_track_details(song)
+                id = track_details['id']  # id de la canción que usamos para analysis
+                
+                # Llamamos a las funciones de genre y analysis
+                track_genre = get_track_genre(id)
+                
+                # Añadimos toda la info en el df
+                df.loc[i, "id"] = track_details['id']
+                df.loc[i,'title_spotify'] = track_details['name']
+                df.loc[i,"album"] = track_details['album']
+                df.loc[i,'sp_popularity'] = track_details['popularity']
+                df.loc[i,"colab"] = ("Y" if len(track_details['artists']) > 1 else "N")
+                df.loc[i,"release_date"] = track_details['release_date']
+                df.loc[i,"danceability"] = track_genre['danceability']
+                df.loc[i,'energy'] = track_genre['energy']
+                df.loc[i,'loudness'] = track_genre['loudness']
+                df.loc[i,'speechiness'] = track_genre['speechiness']
+                df.loc[i,'acousticness'] = track_genre['acousticness']
+                df.loc[i,'instrumentalness'] = track_genre['instrumentalness']
+                df.loc[i,'liveness'] = track_genre['liveness']
+                df.loc[i,'valence'] = track_genre['valence']
+                df.loc[i,'key'] = track_genre['key']
+                df.loc[i,'mode'] = track_genre['mode']
+                df.loc[i,'tempo'] = track_genre['tempo']
+                df.loc[i,'duration']= track_genre['duration']
+            
+                df.to_csv("spotify.csv", index=False)
+                time.sleep(0.1)
+
+            except Exception as e:
+                print(f"Error obteniendo detalles para '{song}': {e}")
+
+    # Convertir la lista de diccionarios en un DataFrame
+    return df
